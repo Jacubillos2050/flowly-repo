2e5a07b669c91ba934c7609816a63814297c2a48
JAIRO A CUBILLOS V <jcubillosvillamil@gmail.com>
2025-12-25 22:14:42 -0500

    feat: add Spring Boot base project with OMS dependencies

---
+++ src/main/java/com/project/flowly/FlowlyApplication.java
@@ -0,0 +1,13 @@
+package com.project.flowly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/flowly/FlowlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.flowly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
